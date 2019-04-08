f360f5620f6caaa675d8d493d687c61208a6d6a9
Guilherme Siqueira <guilhersiqueira@gmail.com>
2019-04-08 09:27:47 -0300

    Update ex1.asm

--- Trabalho 1/ex1.asm
+++ Trabalho 1/ex1.asm
@@ -1,6 +1,11 @@
 .data
-soma: .asciiz "ADD:"
-subtracao: .asciiz "\nSUB:"
+soma: .asciiz "ADD: "
+subtracao: .asciiz "\nSUB: "
+e: .asciiz "\nAND: "
+ou: .asciiz "\nOR: "
+exor: .asciiz "\nXOR: "
+sll1: .asciiz "\nSLL: "
+srl1: .asciiz "\nSRL: "
 
 .text
 main:
@@ -12,7 +17,7 @@ main:
     syscall #chama o SO
     move $s1,$v0 #move a entrada do teclado para o registrador s1
 
-    #printa a string de resultado
+    #printa a string SOMA
     li $v0, 4 #chamada pra ler uma string
     la $a0, soma #mensagem a ser printada armazenada no registrador a0
     syscall #chama o SO
@@ -26,9 +31,54 @@ main:
     la $a0, subtracao #mensagem a ser printada armazenada no registrador a0
     syscall #chama o SO
 
-    sub $a0, $s0, $s1 #subtração
+    sub $a0, $s0, $s1 #subtraÃ§Ã£o
     li $v0, 1 #printa o inteiro na tela
     syscall #chama o SO
-    	
+
+    #printa a string AND
+    li $v0, 4 #chamada pra ler uma string
+    la $a0, e #mensagem a ser printada armazenada no registrador a0
+    syscall #chama o SO
+    
+    and $a0, $s0, $s1 #faz a and    	
+    li $v0, 1 #printa o inteiro no registrador 0
+    syscall #chama o SO
+    
+    #printa a string OR
+    li $v0, 4 #chamada pra ler uma string
+    la $a0, ou #mensagem a ser printada armazenada no registrador a0
+    syscall #chama o SO
+    
+    or $a0, $s0, $s1 #faz a or
+    li $v0, 1 #printa o inteiro no registrador 0
+    syscall #chama o SO
+    
+    #printa a string XOR
+    li $v0, 4 #chamada pra ler uma string
+    la $a0, exor #mensagem a ser printada armazenada no registrador a0
+    syscall #chama o SO
+    
+    xor $a0, $s0, $s1 #faz a xor
+    li $v0, 1 #printa o inteiro no registrador 0
+    syscall #chama o SO
+
+    #printa a string SLL
+    li $v0, 4 #chamada pra ler uma string
+    la $a0, sll1 #mensagem a ser printada armazenada no registrador a0
+    syscall #chama o SO
+    
+    sll $a0, $s0, 3 #faz o sll
+    li $v0, 1 #printa o inteiro no registrador 0
+    syscall #chama o SO
+
+    #printa a string SRL
+    li $v0, 4 #chamada pra ler uma string
+    la $a0, srl1 #mensagem a ser printada armazenada no registrador a0
+    syscall #chama o SO
+    
+    srl $a0, $s1, 1 #faz o srl
+    li $v0, 1 #printa o inteiro no registrador 0
+    syscall #chama o SO
+                                                
     li $v0, 10 #encerra o programa
     syscall #chama o SO
