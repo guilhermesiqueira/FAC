d989ab9a452a6d3b9b892ad85edf307c1c69360c
Guilherme Siqueira <guilhersiqueira@gmail.com>
2019-06-17 12:46:22 -0300

    Dec pra Hex

--- Trabalho 4/t4.asm
+++ Trabalho 4/t4.asm
@@ -12,7 +12,7 @@ main:
   li $t1, 0 # carrega o 0 no registrador t1 (contador do for)
   addi	$t2, $0, '\n'
   jal for
-  jal printaResultado #chama função de printar resultado
+  #jal printaResultado #chama função de printar resultado
   
 # $t3 = d
 # $t4 = s
@@ -23,7 +23,7 @@ main:
 
 for:
   lb $t3, ($a0)
-  beq $t3, $t2, printaResultado  
+  beq $t3, $t2, decToHex  
   and $t3, $t3, 0xFF
   srl $v1,$t8,8 #(c >> 8)
   xor $t4,$t3,$v1 #s = d xor (c >> 8)
@@ -57,19 +57,47 @@ for:
 
   j for
 
-printaResultado:
-  la $a0,str2 #carrega a string str2
-  li $v0,4 #chamada para printar string
-  syscall #chama o SO
+decToHex:
+  li $t0, 0
+  li $t3, 0
+  li $t4, 0
+  li $t2, 0
+  li $t0, 8 #contador
+  la $t3, result #onde a resposta vai ser guardada
+  jal loopHex
 
-  #la $a0, buffer #recarrega o esoaço de bytes para o endereço primário
-  move $a0,$t8 #primary address = t0 
-  li $v0,34 #printa a string
-  syscall #chama o SO
-  jal exit #chama função de sair
+loopHex:
+  beqz $t0, exit
+  rol $t8, $t8, 4 #rotaciona 4 bits para a esquerda
+  and $t4, $t8, 0xf #mascára com 1111
+  ble $t4, 9, Soma #se for menor ou igual a 9, muda pra soma
+  addi $t4, $t4, 55 #se for maior que 9, soma 55
+  b Fim
 
+Soma:
+  addi $t4, $t4, 48 #soma 48 ao resultado
 
+Fim:
+  sb $t4, 0($t3) #guarda o digito do hexadecimal no resultado
+  addi $t3, $t3, 1 #incrementa o contador de endereço
+  addi $t0, $t0, -1 #decrementa o contador do loop
+  j loopHex
+
+# printaResultado:
+#   la $a0,str2 #carrega a string str2
+#   li $v0,4 #chamada para printar string
+#   syscall #chama o SO
+
+#   #la $a0, buffer #recarrega o esoaço de bytes para o endereço primário
+# #  move $a0,$t8 #primary address = t0 
+#   li $v0,34 #printa a string
+#   syscall #chama o SO
+#   jal exit #chama função de sair
+  
 exit:
+  la $a0, result
+  li $v0, 4
+  syscall
   li $v0,10 #Termina o programa
   syscall #chama o SO
 
@@ -77,4 +105,5 @@ exit:
   buffer: .space 16
   str1:  .asciiz "Digite uma string:"
   str2:  .asciiz "CRC16­BUYPASS: "
+  result: .space 8
 
